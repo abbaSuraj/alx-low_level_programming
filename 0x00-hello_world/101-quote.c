dd4c8b515fea91fa517e43ec8774c82adbf0dc85
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-08 12:17:12 -0700

    returning with errors

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function name
+ * printf function
+ * Return: return 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" \- Dora Korpar, 25\-10\-19");
+	return (1);
+}
