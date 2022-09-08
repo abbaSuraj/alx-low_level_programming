08958d61af4d9ee963cca65e9b66c80821517262
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-08 08:17:20 -0700

    printf not puts anymore

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function_name
+ * printf function
+ * Return: return value is 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
