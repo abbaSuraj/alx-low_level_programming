e300a77063a898848735a04857e13abc23b1dd5d
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-08 13:06:34 -0700

    with error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 25-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 25-10-19\n");
 	return (1);
 }
