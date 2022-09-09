3a09dad2fb24e6cdd419c8567b96a951b7d86a64
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-09 13:48:11 -0700

    reverse order for alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 122; p >= 97; p--)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
