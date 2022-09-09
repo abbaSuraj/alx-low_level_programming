e88599eb9fe61f24169a19254bf5ed2e55723e5d
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-09 13:06:19 -0700

    printing numbers using putchar only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar((i % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
