f63de6f99f46117bad1d9bd53331185b96863a28
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-09 03:40:34 -0700

    if and else if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	else
+	{
+		printf("is negative", n);
+	}
+
+	return (0);
+}
