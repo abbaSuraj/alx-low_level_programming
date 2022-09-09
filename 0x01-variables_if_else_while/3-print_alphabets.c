aa3ff8ede6ea19d6cb2182b28e4a7058a13b5af8
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-09 08:15:57 -0700

    lower case and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	for (b = 65; a <= 90; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return(0);
+}
