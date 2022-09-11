006631c923f9ec323d66198f7f2fce174aee60a1
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-11 00:03:23 -0700

    nums separated with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int k;
+	
+	for (k = 0; k < 10; k++)
+	{
+		putchar((k % 10) + '0');
+		if (k != 9);
+		{
+			putchar(44);
+		}
+	}
+	return (0);
+}
