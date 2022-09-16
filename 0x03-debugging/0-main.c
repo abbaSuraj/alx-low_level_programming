bc89c0aecb4d4e688e6bbb5f12f561a7d2498deb
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-16 03:25:34 -0700

    debugged

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - entry point
  * Return: return int  0
  */
-void positive_or_negative(int i);
+
 int main(void)
 {
 	int i;
 
 	i = 98;
-	positive_or_negative(0);
+	positive_or_negative(i);
 
 	return (0);
 }
