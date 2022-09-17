66382a4d241b1953b87add15221fb7e8a951ae9f
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-16 20:35:23 -0700

    dates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,8 +8,8 @@
 #include "main.h"
 
 
-int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
 
