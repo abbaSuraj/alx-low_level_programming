06b930178ec51771cf624a196aaa61662090a12b
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-19 14:13:48 -0700

    string and new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - printing string and newline
+ * @z: First argument
+ *
+ * Description: show length of inputted with new line at end
+ * Return: void
+ */
+void _puts(char *z)
+{
+	while (*z != '\0')
+	{
+		printf("%c\n", *z++);
+	}
+}
