33cde573a7844486eeee62699a0994913191c7b0
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-21 05:53:22 -0700

    remaining days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,6 +11,7 @@
 void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
+int convert_day(int month, int day);
 
 
 #endif /* MAIN_H */
