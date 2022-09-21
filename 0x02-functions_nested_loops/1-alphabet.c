7375372fc532c63fe34d8ff7f6131e8cae4f189c
Salihu Abba Suraj <abbasuraj22@gmail.com>
2022-09-21 06:08:37 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - to print alphabets in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
